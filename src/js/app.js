c0363585fc422cdc729a2d429f44ba4a3049b907
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-03-22 22:52:34 +0000

    agregando iconos de font awesome

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,34 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name} ${variables.lastname}</h1>
-          <h2>${variables.role}</h2>
-          <h3>${variables.city} , ${variables.country}</h3>
+          <h1>${variables.name ? variables.name : " "} ${
+    variables.lastname ? variables.lastname : " "
+  }</h1>
+          <h2>${variables.role ? variables.role : " "}</h2>
+          <h3>${variables.city ? variables.city : " "} , ${
+    variables.country ? variables.country : " "
+  }</h3>
           <ul class=${variables.socialMediaPosition}>
-            <li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/${
+              variables.twitter
+            }"><i class="fa-brands fa-twitter">${
+    variables.twitter ? variables.twitter : " "
+  }</i></a></li>
+            <li><a href="https://github.com/${
+              variables.github
+            }"><i class="fab fa-github">${
+    variables.github ? variables.github : " "
+  }</i></a></li>
+            <li><a href="https://linkedin.com/${
+              variables.linkedin
+            }"><i class="fab fa-linkedin">${
+    variables.linkedin ? variables.linkedin : " "
+  }</i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram
+            }"><i class="fab fa-instagram">${
+    variables.instagram ? variables.instagram : " "
+  }</i></a></li>
           </ul>
         </div>
     `;
